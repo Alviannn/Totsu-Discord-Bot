a4eae28d135a43639a81f93f37f1c97469e85a96
Alviann <alviandq@yahoo.com>
2019-12-10 12:20:58 +0700

    Added changelog commands

---
+++ commands/ChangelogCMD.js
@@ -0,0 +1,31 @@
+const Discord = require('discord.js');
+const fs = require('fs');
+
+module.exports = {
+    name: 'changelog',
+    aliases: ['viewchanges', 'updates', 'changes'],
+    description: 'Sends the changelog that has been committed recently',
+    /**
+     * executes the command
+     * 
+     * @param {Discord.Message} message  the message instance
+     * @param {String[]} args            the arguments 
+     */
+    async execute(message, args) {
+        if (!fs.existsSync('./changelog.txt')) {
+            return message.channel.send('Failed to read the changelog file!');
+        }
+
+        const contentFile = fs.readFileSync('./changelog.txt', {encoding: 'utf8'});
+        if (!contentFile) {
+            return message.channel.send('Failed to read the changelog file!');
+        }
+
+        const embed = new Discord.RichEmbed()
+            .setTitle('Recent changelogs')
+            .setColor('RANDOM')
+            .setDescription('```\n' + contentFile + '\n```');
+
+        message.channel.send(embed);
+    }
+}
