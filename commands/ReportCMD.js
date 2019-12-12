fe3e445a52e98f5e753d1e774406548d6fd74c75
Alviann <alviandq@yahoo.com>
2019-12-12 12:08:52 +0700

    Cleaned up the code a bit

--- commands/ReportCMD.js
+++ commands/ReportCMD.js
@@ -38,9 +38,7 @@ module.exports = {
 
         const attachments = message.attachments;
         const file = attachments.first();
-        const content = message.content;
 
-        // checks if there are any attachments
         if (attachments.size === 0) {
             return message.channel.send('You must provide (an) evidence(s) in form of image (png, jpg, jpeg), gif, or a video (mp4, mkv)!');
         }
@@ -52,35 +50,30 @@ module.exports = {
 
         // gets the report channel id
         const channelId = require('../config.json')['report-channel'];
-        // makes sure that the value isn't empty
+
         if (!channelId) {
             return message.channel.send('Cannot find a report channel to post the report to!');
         }
 
-        // tries to find the channel and check if it exists
         const channel = Main.findChannel(channelId, message.guild);
         if (!channel) {
             return message.channel.send('Cannot find a report channel to post the report to!');
         }
 
-        // checks if the channel is a text channel
         if (!Main.isTextableChannel(channel)) {
             return message.channel.send('Cannot find a report channel to post the report to!'); 
         }
 
-        // checks if the image folder exists, else it'll create the dir for it
         if (!fs.existsSync('./images/')) {
             fs.mkdirSync('./images/');
         }
 
-        // handles download image and do the callback
         await Main.downloadImage(file.url, file.filename, () => {
             // reads the file
             const buffer = fs.readFileSync('./images/' + file.filename);
             // attachs it to the discord attachment
             const attachment = new Discord.Attachment(buffer, file.filename);
     
-            // creates the embed
             const user = message.member.user;
             const embed = new Discord.RichEmbed()
                 .setTitle('Report File')
@@ -91,19 +84,15 @@ module.exports = {
                 .setFooter('Reported by ' + user.username, user.displayAvatarURL);
     
             setTimeout(() => {
-                // deletes the message
                 if (message.deletable) {
                     message.delete(100);
                 }
     
-                // deletes the file
                 fs.unlinkSync('./images/' + file.filename);
             }, 500);
 
-            // debug message to tell if it's downloaded or not
             console.log('Downloaded ' + file.filename);
     
-            // sends the report
             channel.send(embed);
         });
 
