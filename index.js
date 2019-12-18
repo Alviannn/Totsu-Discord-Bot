4c9e913957a141b1429dc72e11726efb7e681b7d
Alviann <alviandq@yahoo.com>
2019-12-18 22:27:01 +0700

    Some changes

--- index.js
+++ index.js
@@ -1,29 +1,21 @@
 const startMillis = new Date().getTime();
 
-// server.js
-// where your node app starts
+// ----------------------- Glitch Stuff ----------------------- //
 
-// init project
 const express = require("express");
 const app = express();
 
-// we've started you off with Express,
-// but feel free to use whatever libs or frameworks you'd like through `package.json`.
-
-// http://expressjs.com/en/starter/static-files.html
 app.use(express.static("public"));
 
-// http://expressjs.com/en/starter/basic-routing.html
 app.get("/", function(request, response) {
   response.sendFile(__dirname + "/views/index.html");
 });
 
-// listen for requests :)
 const listener = app.listen(process.env.PORT, function() {
   console.log("Your app is listening on port " + listener.address().port);
 });
 
-// ------------------------------------------------- //
+// ----------------------- Glitch Stuff ----------------------- //
 
 const fetch = require("superagent");
 const projectName = "totsu-dbt";
